461316fa3dad10247a8cdd91e46cb78ce8cb1310
kslewis19 <60162532+kslewis19@users.noreply.github.com>
2021-05-18 10:20:08 -0400

    initial set up of structure

--- src/RestaurantViewer.js
+++ src/RestaurantViewer.js
@@ -3,6 +3,15 @@ import { TextField, FormLabel } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 
 function RestaurantViewer(props){
+    const [restaurants, setRestaurants]= useState(null)
+
+    const fetchPlaces =()=>{
+
+    }
+    const callback= (results,status)=>{
+        console.log(status)
+    }
+
     return(
         <div>
             This will be a list
